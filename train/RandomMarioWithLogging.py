5e502d03ed80fa93269a21bdf1f355b75b38b4f0
1010110 <write_2_jasim@hotmail.com>
2018-05-23 19:18:27 +1000

    Make script look like it has 20 children experiments

--- train/RandomMarioWithLogging.py
+++ train/RandomMarioWithLogging.py
@@ -5,7 +5,7 @@ import shutil
 
 #save offspring stats
 #ugly name to match VINE examples for now
-def master_extract_cloud_ga(curr_task_results, iteration):
+def master_extract_cloud_ga(allChildrenResults, iteration):
     import csv
     import os
 
@@ -16,63 +16,79 @@ def master_extract_cloud_ga(curr_task_results, iteration):
     filename = "snapshot_offspring_{:04}.dat".format(int(iteration))
     with open(os.path.join(path, filename), 'w+') as file:
         writer = csv.writer(file, delimiter=' ')
-        #for result in curr_task_results:
+        for curr_task_results in allChildrenResults:
             #if (result == "distance"):
             # {'level': 0, 'distance': 6, 'score': 0, 'coins': 0, 'time': 388, 'player_status': 0, 'life': 3}
             
-                #np is NumPy
-        row = np.hstack((curr_task_results.get('distance'),curr_task_results.get('score')))
-        writer.writerow(row)
+            #np is NumPy
+            #looks like VINE wants floating point values.
+            row = np.hstack(("{:.6f}".format(curr_task_results.get('distance')),"{:.6f}".format(curr_task_results.get('score')),"{:.8f}".format(400 -curr_task_results.get('time'))))
+            writer.writerow(row)
     print('Created snapshot:' + filename)
 
 #save parent stats
 #same ugly name issue for now
-def master_extract_parent(iteration):
+def master_extract_parent(allChildrenResults, iteration):
     import os
-    path = "snapshots/snapshot_gen_{:04}/".format(int(iteration))
+    #we will take the first result and pretend it is the parent
+    path = "snapshots/snapshot_gen_{:04}/".format(int(iteration +1))
     if not os.path.exists(path):
         os.makedirs(path)
 
-    filename = "snapshot_parent_{:04}.dat".format(int(iteration))    
-  
-    previousPath = "snapshots/snapshot_gen_{:04}/".format(int(iteration - 1))
-    previousFilename = "snapshot_offspring_{:04}.dat".format(int(iteration -1))
+    filename = "snapshot_parent_{:04}.dat".format(int(iteration +1))    
+    with open(os.path.join(path, filename), 'w+') as file:
+        writer = csv.writer(file, delimiter=' ')
+        for curr_task_results in allChildrenResults:
+            #if (result == "distance"):
+            # {'level': 0, 'distance': 6, 'score': 0, 'coins': 0, 'time': 388, 'player_status': 0, 'life': 3}
+            
+            #np is NumPy
+            #looks like VINE wants floating point values.
+            row = np.hstack(("{:.6f}".format(curr_task_results.get('distance')),"{:.6f}".format(curr_task_results.get('score')),"{:.8f}".format(400 -curr_task_results.get('time'))))
+            writer.writerow(row)
+            break #just need the first
+    print('Created parent snapshot:' + filename)
     
-    shutil.copy2(previousPath + previousFilename, path + filename) 
-    print('Created snapshot:' + filename)
 
 env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
 experiment = 0;
 
 #loop experiments forever
-while True:    
-    observation = env.reset()
-    done = False
-    t = 0
-    lastDistance = 0;
-    strike = 0;
-    while not done:
-        action = env.action_space.sample()  # choose random action
-        observation, reward, done, info = env.step(action)  # feedback from environment    
-        t += 1
-        
-        if not t % 100:
-            print(t, info)
+while True: 
 
-        #add cutoff to none progressing random agents
-        if (info.get('distance') < lastDistance):
-            strike +=1
-        else:        
-            strike = 0
+    #pretend we have 20 children
+    childRun = 0
+    experimentInfos = [] #store all the child results
+    while (childRun < 20):    
+        observation = env.reset()
+        done = False
+        t = 0
+        lastDistance = 0;
+        strike = 0;
+        while not done:
+            action = env.action_space.sample()  # choose random action
+            observation, reward, done, info = env.step(action)  # feedback from environment    
+            t += 1
+            
+            if not t % 100:
+                print(t, info)
 
-        if (strike > 3):
-            done = True
-        
-        lastDistance = info.get('distance')
+            #add cutoff to none progressing random agents
+            if (info.get('distance') < lastDistance):
+                strike +=1
+            else:        
+                strike = 0
 
-    master_extract_cloud_ga(info,experiment)
-    if (experiment > 0):
-        master_extract_parent(experiment)
+            if (strike > 3):
+                done = True
+            
+            lastDistance = info.get('distance')
+        childRun += 1
+        print("Mock Child Run: {} of Gen: {} completed.".format(childRun ,experiment))
+        experimentInfos.append(info)
+
+    master_extract_cloud_ga(experimentInfos,experiment)
+    master_extract_parent(experimentInfos,experiment)
 
     experiment += 1
             
