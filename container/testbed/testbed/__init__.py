5783287804db2269093c188a2b129359d6e8c318
jbeemster <joshua.a.beemster@gmail.com>
2018-08-12 17:56:09 +0200

    Add common NEAT functions

--- container/testbed/testbed/__init__.py
+++ container/testbed/testbed/__init__.py
@@ -1,4 +1,5 @@
 """testbed: __init__.py declaration"""
 
 import testbed.logging
+import testbed.training
 import testbed.version
---
+++ container/testbed/testbed/training/__init__.py
@@ -0,0 +1,3 @@
+"""testbed.training: __init__.py declaration"""
+
+import testbed.training.neat
