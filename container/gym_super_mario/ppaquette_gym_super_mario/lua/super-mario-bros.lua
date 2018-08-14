99e87699c0145c61f0bcde9d795dc14828b3fdca
Jasim Schluter <write_2_jasim@hotmail.com>
2018-08-14 23:09:45 +1000

    remove old test.fcs in lua folder make lua debug message more obvious for now

--- container/gym_super_mario/ppaquette_gym_super_mario/lua/super-mario-bros.lua
+++ container/gym_super_mario/ppaquette_gym_super_mario/lua/super-mario-bros.lua
@@ -411,6 +411,7 @@ end
 
 function load_saved_state_from_disk(folder, level, distance)   
     gui.text(50,50, "load_saved_state_from_disk called:" .. folder .. level .. distance);
+	emu.pause(); --make it obvious there is an error
     filename = pick_closest_file(folder);
 	gui.text(50,50, "File to load: " .. filename);
         emu.pause(); --make it obvious there is an error
