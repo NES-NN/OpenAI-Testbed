66927f9253f58d7fc9370272eaa700f7dec8afb7
Loic Nyssen <loic@nyssen.com.au>
2018-08-25 14:08:54 +0200

    Agent withrestore (#42)
    * Added ability to load a saved network
    
    * fixed evaluate function. 
    
    A few things were out of order.
    
    * Moved getting env out of evaluate. 
    
    It was causing errors to be flooded to the console every time it would eval a genome. now it only happens once when the script is loaded.
    
    * Removed redundant setting of global variables  
    
    They are set it argsparse.
    
    * Removed unnecessary variable declarations
    
    * Actually exits when the last gen meets distance threshold 
    
    Its was only looking at the first network.
    
    * Small fix..

--- train/NEAT/agent.py
+++ train/NEAT/agent.py
@@ -13,10 +13,11 @@ from testbed.training import neat as neat_
 # -----------------------------------------------------------------------------
 
 GYM_NAME = 'ppaquette/SavingSuperMarioBros-1-1-Tiles-v0'
-STATE_PATH = None
-START_DISTANCE = 40
-END_DISTANCE = 100
 
+
+# -----------------------------------------------------------------------------
+#  HELPERS
+# -----------------------------------------------------------------------------
 
 def get_env():
     env = gym.make(GYM_NAME)
@@ -25,35 +26,40 @@ def get_env():
     return env
 
 
+# -----------------------------------------------------------------------------
+#  NEAT TRAINING
+# -----------------------------------------------------------------------------
+
 def evaluate(genome, config):
-    env = get_env()
     net = neat.nn.FeedForwardNetwork.create(genome, config)
 
     done = False
     stuck = 0
     stuck_max = 600
-    info = {}
 
-    while not done or info['distance'] < END_DISTANCE:
-        env.loadSaveStateFile(START_DISTANCE)
-        observation = env.reset()
+    ENV.loadSaveStateFile(START_DISTANCE)
+    observation = ENV.reset()
 
+    while not done:
         outputs = neat_.clean_outputs(net.activate(observation.flatten()))
-        observation, reward, done, info = env.step(outputs)
+        observation, reward, done, info = ENV.step(outputs)
         stuck += 1 if reward <= 0 else 0
 
         # TODO: Needs improvement, need to disable at end of level and when in a pipe.
-        if stuck > stuck_max:
+        # Also not sure what will happen with END_DISTANCE when in a pipe..
+        if stuck > stuck_max or info['distance'] > END_DISTANCE:
             break
-       # if info['life'] == 0:
-       #     break
 
-        env.close()
+    ENV.close()
+
     return neat_.calculate_fitness(info)
 
 
-def evolve(config, num_cores):
-    pop = neat.Population(config)
+def evolve(config, num_cores, checkpoint):
+    if checkpoint:
+        pop = neat.Checkpointer.restore_checkpoint(checkpoint)
+    else:
+        pop = neat.Population(config)
 
     pop.add_reporter(neat.Checkpointer(1, 600))
     pop.add_reporter(neat.StdOutReporter(True))
@@ -71,9 +77,10 @@ def evolve(config, num_cores):
         # Save the best Genome from the last 5 gens.
         with open('Best-{}.pkl'.format(len(stats.most_fit_genomes)), 'wb') as output:
             pickle.dump(winner, output, 1)
-        
-        if stats.get_fitness_mean()[0] > END_DISTANCE:
+
+        if stats.get_fitness_mean()[-1] >= END_DISTANCE:
             break
+
 
 def main():
     parser = argparse.ArgumentParser(description='Mario NEAT Agent Trainer')
@@ -83,10 +90,12 @@ def main():
                         help="The number of cores on your computer for parallel execution")
     parser.add_argument('--state-path', type=str, default="/opt/train/stateSaving/saveStates/",
                         help="The path to the state file to commence training from")
-    parser.add_argument('--input-distance', type=int, default=570,
+    parser.add_argument('--input-distance', type=int, default=40,
                         help="The target distance Mario should start training from")
     parser.add_argument('--target-distance', type=int, default=1000,
                         help="The target distance Mario should achieve before closing")
+    parser.add_argument('--checkpoint', type=str, default="",
+                        help="Resume training from a saved checkpoint")
     args = parser.parse_args()
 
     # Setup logger
@@ -103,9 +112,11 @@ def main():
     START_DISTANCE = args.input_distance
     global END_DISTANCE
     END_DISTANCE = args.target_distance
+    global ENV
+    ENV = get_env()
 
     # Evolve!
-    evolve(config=config, num_cores=args.num_cores)
+    evolve(config=config, num_cores=args.num_cores, checkpoint=args.checkpoint)
 
 
 if __name__ == '__main__':
